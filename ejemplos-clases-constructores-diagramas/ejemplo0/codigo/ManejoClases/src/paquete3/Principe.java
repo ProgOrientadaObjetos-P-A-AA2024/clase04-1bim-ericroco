cc2b2cdf3a3343f332931253488b636af1d078c5
ericroco <erickrodas559@gmail.com>
2024-05-01 16:37:05 -0500

    Clase04

--- ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principe.java
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principe.java
@@ -9,5 +9,13 @@ package paquete3;
  * @author utpl
  */
 public class Principe {
-    
+
+    public static void main(String[] args) {
+        /* En esta clase se ha creado un objeto del tipo computadora, se usa un
+        constructor por defecto, ya que la clase no tiene ningun constructor
+        declarado.
+        */
+        Computadora computadoraTrabajo = new Computadora();
+        
+    }
 }
