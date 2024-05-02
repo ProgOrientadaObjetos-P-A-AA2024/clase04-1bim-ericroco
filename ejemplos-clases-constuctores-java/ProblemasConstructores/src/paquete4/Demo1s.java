41d3a4fd01c156073020dd706ec33b3f6121d63f
ericroco <erickrodas559@gmail.com>
2024-05-02 15:57:46 -0500

    clase04

---
+++ ejemplos-clases-constuctores-java/ProblemasConstructores/src/paquete4/Demo1s.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete4;
+
+/**
+ *
+ * @author utpl
+ */
+public class Demo1s {
+    
+    public static void main(String[] args){
+        
+    }
+}
