ac506e9d9d7cb5490e7393f362319eed01351415
ericroco <erickrodas559@gmail.com>
2024-05-02 16:24:52 -0500

    clase04

--- ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
+++ ejemplos-clases-constuctores-java/EjemploConstructores/src/paquete7/FacturaTelefonica.java
@@ -60,5 +60,14 @@ public class FacturaTelefonica {
     public double obtenerValorFactura() {
         return valorFactura;
     }
-
+    @Override
+    public String toString(){
+        String cadena = String.format("Número telefonico: %s\n"
+                + "Minutos mes: %.2f\nValor minuto: %.2f\n"
+                + "Valor factura: %.2f\n", obtenerNumeroTelefono(),
+                obtenerMinutosMes(),
+                obtenerValorMinuto(),
+                obtenerValorFactura());
+        return cadena;
+    }
 }
